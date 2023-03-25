3930643983078a5ff079d56be794c215eb560a2b
Kendall Arneaud <kendall.arneaud@gmail.com>
2023-03-25 17:23:24 +0000

    add countdown and modify game mechanic animatons

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -1,67 +1,93 @@
+import Countdown from "./countdown";
+
 const $charless = document.querySelectorAll('.charles');
 const touchClick = ('ontouchstart' in window) ? 'touchstart' : 'click';
+// Create a new Countdown object with an element selector.
+const countdown = new Countdown('#countdown');
+
+// Register event listeners for the various events.
+countdown.element.addEventListener('started', event => console.log(`Countdown started with ${event.detail} seconds`));
+countdown.element.addEventListener('paused', event => console.log(`Countdown paused with ${event.detail} seconds remaining`));
+countdown.element.addEventListener('stopped', event => console.log(`Countdown stopped with ${event.detail} seconds total`));
+
+$charless.forEach($s => {
+    let closing = false;
+    let clicked = false;
+    let intvl1, intvl2;
+    $s.addEventListener(touchClick, (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        if (!clicked && !closing) {
+            clearTimeout();
+            closing = true;
+            clicked = true;
+            //$s.classList.add('clicked');
+            $s.dispatchEvent(new Event('close:item'));
+            console.log(`${$s.id} has beend clicked ${clicked} and is now closing ${closing}`);
+        }
+
+        return false;
+    });
+
+    $s.addEventListener('open:item', () => {
+        let $bh = $s.querySelector('.bighead');
+        $bh.classList.add('up');
+        intvl1 = setTimeout(() => {
+            $s.dispatchEvent(new Event('close:item'));
+            clearTimeout(intvl1);
+        }, Math.random() * 2500);
+        console.log(`Hi its me ${$s.id}`)
+    });
+
+    $s.addEventListener('close:item', () => {
+        closing = true;
+        let $bh = $s.querySelector('.bighead');
+        $bh.classList.remove('up');       
+        $bh.addEventListener('transitionend', onTransitionEnd);
+        // $s.classList.remove('clicked');
+        //$bh.classList.add('down');
+    });
+
+    const onTransitionEnd = () => {
+        let $bh = $s.querySelector('.bighead');
+        closing = false;
+        //$bh.classList.remove('down');
+        intvl2 = setTimeout(() => {
+            clicked = false;
+            $s.dispatchEvent(new Event('open:item'));
+            clearTimeout(intvl2)
+        }, Math.random() * 2500);
+        console.debug($s.id, 'transition end',' clicked ', clicked, ' closing: ', closing);
+        $bh.removeEventListener('transitionend', onTransitionEnd);
+    };
+
+    $s.addEventListener('clear:intvl', () => {
+        let $bh = $s.querySelector('.bighead');
+        clearTimeout(intvl);
+        closing = true;
+        clicked = false;
+        $bh.classList.remove('up', 'click', 'down');
+        //$bh.classList.add('down');
+    });
+        
+});
 
 export default {
     start() {
-        $charless.forEach($s => {
-            let closing = false;
-            let clicked = false;
-            let intvl;
+
+        //$charless[1].dispatchEvent(new Event('open:item'));
+
+        $charless.forEach((e,i) => {
+            setTimeout(() => e.dispatchEvent(new Event('open:item')), Math.random() * 1000)
+            countdown.element.addEventListener('ended', () => {
+                // e.dispatchEvent(new Event('clear:intvl'))
+            })    
+        })
+
+        countdown.start(90);
         
-            $s.addEventListener(touchClick, () => {
-            console.log('closing', closing, 'clicked', clicked);
-        
-            if (!clicked && !closing) {
-                closing = true;
-                clicked = true;
-                clearTimeout(intvl);
-                $s.classList.add('click');
-                $s.dispatchEvent(new Event('close:item'));
-                $scoreText.innerHTML = ++score;
-            }
-        
-            return false;
-            });
-        
-            $s.addEventListener('open:item', () => {
-            $s.classList.add('open');
-            intvl = setTimeout(() => {
-                $s.dispatchEvent(new Event('close:item'));
-            }, Math.random() * 2500);
-            });
-        
-            $s.addEventListener('close:item', () => {
-            closing = true;
-            clearInterval(intvl);
-            $s.classList.remove('open');
-            $s.classList.add('close');
-            $s.addEventListener('transitionend', onTransitionEnd);
-            });
-        
-            const onTransitionEnd = () => {
-            $s.removeEventListener('transitionend', onTransitionEnd);
-            if (!closing) return false;
-            else if (intvl) clearInterval(intvl);
-        
-            closing = false;
-            clicked = false;
-            $s.classList.remove('close', clicked ? 'click' : '');
-            intvl = setTimeout(() => {
-                $s.dispatchEvent(new Event('open:item'));
-            }, Math.random() * 2500);
-            };
-        
-                $s.addEventListener('clear:intvl', () => {
-                clearInterval(intvl);
-                closing = true;
-                clicked = false;
-                $s.classList.remove('open', 'click');
-                $s.classList.add('close');
-                });
-                
-        });
     },
     pause(){
-
+        countdown.pause();
     }
 }
