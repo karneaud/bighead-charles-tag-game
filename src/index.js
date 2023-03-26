69a181679e3bd3f6f84125c8f491a1ec9d8e801f
Kendall Arneaud <kendall.arneaud@gmail.com>
2023-03-26 21:06:25 +0000

    add tally points logic

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ if(process.env.NODE_ENV != "production") {
 (($) => {
 
     const  
+        $score = $.document.querySelector('#points'),
         countdown = new Countdown('#countdown'),
         Game = new Singleton(), toggleMsgs = (msg) => {
             $currentMsg.classList.remove('show')
@@ -24,7 +25,7 @@ if(process.env.NODE_ENV != "production") {
                         Game.resume(); break;
                     case 'finished':
                     case false:
-                        countdown.start(10);
+                        countdown.start(30);
                         Game.start(); break;
                     default: break; 
                 }
@@ -32,29 +33,34 @@ if(process.env.NODE_ENV != "production") {
                 return false
             },
             scrollTop: true
+        }, setScore = (s) => {
+            s = s === false ? -1 : parseInt($score.textContent)
+            $score.textContent = s + 1
         };
         
     // Register event listeners for the various events.
     countdown.element.addEventListener('paused', event => Game.pause );
     countdown.element.addEventListener('ended', event => {
-        console.log('end game')
         Game.finish()
     } ); 
 
     var $currentMsg = $.document.querySelector('.show.message');
     $.tinyModal = new tinyModal(elem, opts);
+    $.document.addEventListener('charles:clicked',(e) => {
+       setScore(1);
+    })
     $.document.addEventListener('gameStateChange',(e) => {
-        console.log(e.detail.currentState, 'gameStateChange')
         switch (e.detail.currentState)
         {
             case 'finished':
             case 'paused':
                 toggleMsgs(e.detail.currentState);
+                setScore(false);
                 $.tinyModal.show(); break;
             case 'resume':
                 countdown.start(countdown.secondsRemaining); break;
             default: if($.tinyModal.isShown) $.tinyModal.hide(); break; 
         }
     });
-    $.tinyModal.show();
+   $.tinyModal.show();
 })(window)
