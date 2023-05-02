c7a4d9af58fbb24b3857ed81285a0fe7843845ab
karneaud <kendall.arneaud@gmail.com>
2023-05-02 16:15:02 +0000

    Add logic to update nickname and save meta data. use env variables. Store info in localStorage

--- src/index.js
+++ src/index.js
@@ -17,38 +17,52 @@ if (process.env.NODE_ENV == "production") {
 
 (($) => {
   // Define the power curve parameters
-  const saveScore = () => {
-    if (!playerInfo) return toggleMsg("profile")
-
-    $.galaxy.ClientAPI.ReportScore({
-      leaderboard_id: '90dd7707-9a0e-4944-9f1f-f432e685b8f4',
-      score: 11 //Game.getScore()
-    }, (r, e) => {
-      console.log('score', r, e)
-    })
-  }, saveProfile = () => {
-    var name = $playerNameInpt.value, age = $playerAgeInpt.value
-    if (!name || !age) return toggleMsg('profile');
-
-    $.galaxy.ClientAPI.UpdatePlayerProfile(
-      {
-        "Nickname": name
-      }, (r, e) => {
-        console.log('player', r)
-        r.data.player_profile.state = { age }
-        $.localStorage.setItem('player', JSON.stringify(r.data.player_profile))
-        playerInfo = r.data.player_profile
-        toggleMsgs('finished')
+  const LEADERBOARD_ID = $.LEADERBOARD_ID || process.env.LEADERBOARD_ID,
+    setPlayerName = (name) => 
+    {
+      name = name.substr(0,name.indexOf(' ') || name.length )
+      $userName.forEach(e => {
+        e.textContent = ` ${name}`
       })
-    playerInfo = { nickname: name, state: { age } }
-    $.galaxy.ClientAPI.SaveState({
-      state:  { Age: 11 }
-    },(r,e) => console.log('state',r,e))
-    saveScore()
+    },saveScore = () => {
+      if (!playerInfo) return toggleMsg("profile")
+  
+      $.galaxy.ClientAPI.ReportScore({
+        leaderboard_id: LEADERBOARD_ID,
+        score: Game.getScore()
+      }, (r, e) => {
+        toggleMsg('scores')
+      })
+    }, saveProfile = () => {
+      var name = $playerNameInpt.value, age = $playerAgeInpt.value
+      if (!name || !age) return ;
+  
+      $.galaxy.ClientAPI.UpdatePlayerProfile(
+        {
+          "Nickname": name
+        }, (r, e) => 
+          {
+            $formBtn.textContent = 'Update'
+            $formBtn.classList.remove('submitting')
+            if(Game.getScore() > 0) saveScore() 
+
+            if(e) return ;
+            
+            r.data.player_profile.state = { age }
+            $.localStorage.setItem('player', JSON.stringify(r.data.player_profile))
+            playerInfo = r.data.player_profile           
+      })
+      $.galaxy.ClientAPI.SaveState({
+        state:  { Age: 11 }
+      },(r,e) => {
+        
+      })
+      playerInfo = { nickname: name, state: { age } }
+      setPlayerName(name)
   }, minInterval = 1000, // Minimum interval in milliseconds
-    maxInterval = 3750, // Maximum interval in milliseconds
+    maxInterval = $.MAX_SPEED ? parseInt($.MAX_SPEED) : 3750, // Maximum interval in milliseconds
     minSeconds = 10, // Minimum remaining seconds for maximum speedup
-    maxSeconds = 15, // Maximum remaining seconds for minimum speedup
+    maxSeconds = $.MAX_TIMELIMIT ?parseInt($.MAX_TIMELIMIT) : 45, // Maximum remaining seconds for minimum speedup
     power = 2,
     $score = $.document.querySelector('#points'),
     $total = $.document.querySelector('#total'),
@@ -64,7 +78,7 @@ if (process.env.NODE_ENV == "production") {
         switch (Game.getState()) {
           case 'paused':
             Game.resume(); break;
-          case 'finished':
+          case 'finished': 
           case false:
             countdown.start(maxSeconds);
             Game.start();
@@ -97,7 +111,7 @@ if (process.env.NODE_ENV == "production") {
 
   var $currentMsg = $.document.querySelector('.show.message'), $playerNameInpt = $.document.querySelector('input[name=player_name]'), $playerAgeInpt = $.document.querySelector('input[name=player_age]'),
     $formBtn = $.document.querySelector('form[name=profile-form] > button'),
-    playerInfo = $.localStorage.getItem('player') || false;
+    playerInfo = $.localStorage.getItem('player') || false, $userName = $.document.querySelectorAll('.user_name');
   playerInfo = playerInfo ? JSON.parse(playerInfo) : false;
   $.defaultInterval = 2000; // 1 second
   $.tinyModal = new tinyModal(elem, opts);
@@ -109,6 +123,8 @@ if (process.env.NODE_ENV == "production") {
   $.document.addEventListener('gameStateChange', (e) => {
     switch (e.detail.currentState) {
       case 'finished':
+        toggleMsgs(e.detail.currentState)
+        $.tinyModal.show()
         break;
       case 'paused':
         toggleMsgs(e.detail.currentState);
@@ -122,27 +138,30 @@ if (process.env.NODE_ENV == "production") {
     e.preventDefault()
     $formBtn.setAttribute('disabled', 'disabled')
     $formBtn.classList.add('submitting')
-    $formBtn.textContent = "Sending.."
+    $formBtn.textContent = "Sending..."
     saveProfile()
   })
   $.tinyModal.show();
   $.toggleMsg = toggleMsgs;
   $.showLeaderboard = () => {
     $.galaxy.ShowLeaderboard({
-      leaderboard_id: '90dd7707-9a0e-4944-9f1f-f432e685b8f4'
+      leaderboard_id: LEADERBOARD_ID
     })
   }
-  $playerNameInpt.value = playerInfo ? playerInfo.nickname : ''
-  $playerAgeInpt.value = playerInfo ? playerInfo.state.age : ''
   $.galaxy.InitSDK({
-    publishableKey: 'c952f485-e38d-4406-8e2a-8ff42d0f80e9',
+    publishableKey: process.env['PUBLIC_KEY'],
   }, (result) => {
     console.log('init', result)
   }, (error) => {
     console.error('not ok', error)
   });
 
-  setTimeout(() => saveScore(), 6000);
   preload.fetch(['./assets/logo.png', './assets/background.png', './assets/bush2.png', './assets/bush3.png', './assets/ch1.png', './assets/ch3.png', './assets/ch4.png']
   );
+  if(playerInfo) {
+      setPlayerName(playerInfo.nickname )
+      $formBtn.textContent = 'Update';
+      $playerNameInpt.value = playerInfo.nickname
+      $playerAgeInpt.value = playerInfo.state.age
+        }
 })(window)
